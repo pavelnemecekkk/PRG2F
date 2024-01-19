9dcd950fc8abfb8b06746bb4a2db4dfe5b91e305
pavel.nemecek <fenrix667@gmail.com>
2024-01-19 12:22:31 +0100

    Dvojrozměrné pole

---
+++ src/MultipleArray/MultipleArray.java
@@ -0,0 +1,23 @@
+package MultipleArray;
+
+public class MultipleArray {
+    public static void main(String[] args) {
+
+
+        // Zápis x[][]{
+        // {x,x,x},
+        // {y,y,y},
+        // {z,z,z}
+        // }
+        // můžeme tam napsat více polí
+
+
+        int[][] array2 = {
+                {1,2,3},
+                {4,5,6},
+                {3,99,64}
+        };
+
+        System.out.println(array2[1][2]);
+    }
+}
