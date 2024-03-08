8b5eb287e1db05a7d9177b86b138d8de201f7b95
pavel.nemecek <fenrix667@gmail.com>
2024-03-08 12:41:06 +0100

    Methody

--- src/cvika/AdvancedLoops.java
+++ src/cvika/AdvancedLoops.java
@@ -13,17 +13,31 @@ public class AdvancedLoops {
 //        }
 //        System.out.println("Happy ending");
 
-        int dice1 = (int)(Math.random()*6+1);
-        int dice2 = (int)(Math.random()*6+1);
-        int counter = 1;
-        System.out.println("K1 = " + dice1 + "K2 = " + dice2);
-        while (dice1 + dice2 < 12){
-            dice1 = (int)(Math.random()*6+1);
-            dice2 = (int)(Math.random()*6+1);
-            System.out.println("K1 = " + dice1 + " K2 = " + dice2);
-            counter++;
-        }
-        System.out.println("Hazel jsi " + counter);
+//        int dice1 = (int)(Math.random()*6+1);
+//        int dice2 = (int)(Math.random()*6+1);
+//        int counter = 1;
+//        System.out.println("K1 = " + dice1 + "K2 = " + dice2);
+//        while (dice1 + dice2 < 12){
+//            dice1 = (int)(Math.random()*6+1);
+//            dice2 = (int)(Math.random()*6+1);
+//            System.out.println("K1 = " + dice1 + " K2 = " + dice2);
+//            counter++;
+//        }
+//        System.out.println("Hazel jsi " + counter);
 
+
+
+
+       int tails = 0;
+       int heads = 0;
+        for (int i = 0; i < 101; i++) {
+           int num = (int) (Math.random() * 2 + 1);
+           if (num == 1 ){
+               tails++;
+           }else {
+               heads++;
+           }
+        }
+        System.out.println("Padl orel" + " " + tails + " " + "Padlo hlava" + " " + heads);
     }
 }
--- src/methods/practice.java
+++ src/methods/practice.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 
 public class practice {
 
-    public static int randomnum(int max, int min){
-        return (int) (Math.random() * (max-min+1) + min);
+    public static int randomnum(int max, int min) {
+        return (int) (Math.random() * (max - min + 1) + min);
     }
 
 
-    public static int[] randomArray(int size, int min, int max){
+    public static int[] randomArray(int size, int min, int max) {
         int[] array = new int[size];
         for (int i = 0; i < array.length; i++) {
             array[i] = randomnum(max, min);
@@ -19,23 +19,21 @@ public class practice {
 
     //time convert
 
-    public static String minuteconvert(int seconds){
+    public static String minuteconvert(int seconds) {
         String time;
-        int min;
-        int sec;
+        int hours = seconds / 3600;
+        int remainingSeconds = seconds - (hours * 3600);
+        int min = remainingSeconds / 60;
+        int sec = remainingSeconds % 60;
 
 
-
-        return time;
+        return hours + ":" + min + ":" + sec;
     }
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println(randomnum(-2, 2));
-
         }
-        System.out.println(Arrays.toString(randomArray(10,50,100)));
+        System.out.println(minuteconvert(3662));;
     }
-
-
 }
